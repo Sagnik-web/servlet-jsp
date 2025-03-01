5093e22f56ae87be9e6449e158fb5fb52328f4c3
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2025-03-01 13:56:48 +0530

    solv prblem

--- src/main/java/myweb/sb/register/dao/RegisterDao.java
+++ src/main/java/myweb/sb/register/dao/RegisterDao.java
@@ -7,17 +7,32 @@ public class RegisterDao {
 
 	
 	public void userRegister(Register register) throws ClassNotFoundException {
-//		String INSERT_DATA = "insert into register(name,email,password,phone_no,address) values(?,?,?,?,?);";
+		String INSERT_DATA = "insert into register(name,email,password,phone_no,address) values(?,?,?,?,?);";
 		
 		Class.forName("org.postgresql.Driver");
 		
-		try(Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydb?useSSL=false", "postgres", "1234")){
-			System.out.print("Hi "+register.getName());
+		try(Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydb?useSSL=false", "postgres", "1234");
+				PreparedStatement ps = conn.prepareStatement(INSERT_DATA)	){
+			
+			
+			ps.setString(1, register.getName());
+			ps.setString(2, register.getEmail());
+			ps.setString(3, register.getPassword());
+			ps.setString(4, register.getPhone_no());
+			ps.setString(5, register.getAddress());
+	
+			int result = ps.executeUpdate();
+			
+			if(result == 1) {
+				System.out.println("Data Inserted Successfully.");
+			}
+			
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
 		
 //		System.out.print("Hi "+register.getName());
+		
 	}
 	
 }
